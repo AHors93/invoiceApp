7206a95f9ddf574948e352f2948edea69c100bf8
Adam Horscraft <adam.horscraft@foodhub.co.uk>
2021-02-24 17:27:55 +0000

    tidied up and running in React

--- src/app.tsx
+++ src/app.tsx
@@ -1,71 +1,40 @@
-import * as ReactDOM from "react-dom";
-import * as React from "react";
+import React from "react";
 
-class App extends React.Component {
-  state: { name: string; details: string; amount: any };
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      name: "",
-      details: "",
-      amount: null,
-    };
-  }
-
-  handleName = (e) => {
-    this.state({
-      name: e.target.value,
-    });
-  };
-  handleDetails = (e) => {
-    this.state({
-      details: e.target.value,
-    });
-  };
-  handleAmount = (e) => {
-    this.state({
-      amount: e.target.value,
-    });
-  };
-
-  componentDidMount = async () => {
-    await fetch("http://localhost:8080")
-      .then((response) => response.json())
-      .then((data) => this.setState({ list: data.data }));
-  };
-  render() {
-    return (
-      <div>
+export const App = () => {
+  return (
+    <React.Fragment>
+      <div class="wrapper">
         <h1>Invoice Logger</h1>
 
         <ul class="item-list"></ul>
-        <footer>
-          <form class="new-item-form">
-            <div class="field">
-              <label>Type:</label>
-              <select id="type">
-                <option value="invoice">Invoice</option>
-                <option value="payment">Payment</option>
-              </select>
-            </div>
-            <div class="field">
-              <label>To / From:</label>
-              <input type="text" id="tofrom" />
-            </div>
-            <div class="field">
-              <label>Details:</label>
-              <input type="text" id="details" />
-            </div>
-            <div class="field">
-              <label>Amount (£):</label>
-              <input type="number" id="amount" />
-            </div>
-            <div>
-              <button id="addbutton">Add</button>
-            </div>
-          </form>
-        </footer>
       </div>
-    );
-  }
-}
+
+      <footer>
+        <form class="new-item-form">
+          <div class="field">
+            <label>Type:</label>
+            <select id="type">
+              <option value="invoice">Invoice</option>
+              <option value="payment">Payment</option>
+            </select>
+          </div>
+          <div class="field">
+            <label>To / From:</label>
+            <input type="text" id="tofrom" />
+          </div>
+          <div class="field">
+            <label>Details:</label>
+            <input type="text" id="details" />
+          </div>
+          <div class="field">
+            <label>Amount (£):</label>
+            <input type="number" id="amount" />
+          </div>
+          <div>
+            <button id="addbutton">Add</button>
+          </div>
+        </form>
+      </footer>
+    </React.Fragment>
+  );
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackRootPlugin = require("html-webpack-root-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app.js",
+  mode: "development",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./public",
+    hot: true,
+  },
+  entry: "./src/index.tsx",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      inject: true,
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
   },
   module: {
-    rules: [{ test: /\.(png|svg|jpg|jpeg|gif)$/i, use: "raw-loader" }],
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, use: "raw-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
